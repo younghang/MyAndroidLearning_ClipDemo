9de9144fe652e18a17da68282399bf34931fe01e
younghang <1179536490@qq.com>
2016-12-05 21:25:21 +0800

    add a file choose

--- app/src/main/java/com/example/yanghang/myapplication/ActivityMessage.java
+++ app/src/main/java/com/example/yanghang/myapplication/ActivityMessage.java
@@ -1,13 +1,22 @@
 package com.example.yanghang.myapplication;
 
+import android.app.Activity;
 import android.content.ClipboardManager;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.yanghang.myapplication.ListPackage.MessageList.MessageAdapter;
@@ -20,13 +29,91 @@ import java.util.List;
 
 public class ActivityMessage extends SwipeBackActivity {
 
+    private final static int REQUEST_FILE = 678;
     public static String DIALOG_MESSAGE = "dialog_message";
-
     Toolbar toolbar;
     RecyclerView recyclerView;
     MessageAdapter messageAdapter;
     List<MessageData> messageData;
     private SwipeBackLayout mSwipeBackLayout;
+    private ImageButton btnSend;
+    private ImageButton btnImage;
+    private ImageButton btnFile;
+
+    public static String getPhotoPathFromContentUri(Context context, Uri uri) {
+        String photoPath = "";
+        if (context == null || uri == null) {
+            return photoPath;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, uri)) {
+            String docId = DocumentsContract.getDocumentId(uri);
+            if (isExternalStorageDocument(uri)) {
+                String[] split = docId.split(":");
+                if (split.length >= 2) {
+                    String type = split[0];
+                    if ("primary".equalsIgnoreCase(type)) {
+                        photoPath = Environment.getExternalStorageDirectory() + "/" + split[1];
+                    }
+                }
+            } else if (isDownloadsDocument(uri)) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                photoPath = getDataColumn(context, contentUri, null, null);
+            } else if (isMediaDocument(uri)) {
+                String[] split = docId.split(":");
+                if (split.length >= 2) {
+                    String type = split[0];
+                    Uri contentUris = null;
+                    if ("image".equals(type)) {
+                        contentUris = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("video".equals(type)) {
+                        contentUris = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("audio".equals(type)) {
+                        contentUris = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    }
+                    String selection = MediaStore.Images.Media._ID + "=?";
+                    String[] selectionArgs = new String[]{split[1]};
+                    photoPath = getDataColumn(context, contentUris, selection, selectionArgs);
+                }
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            photoPath = uri.getPath();
+        } else {
+            photoPath = getDataColumn(context, uri, null, null);
+        }
+
+        return photoPath;
+    }
+
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        String column = MediaStore.Images.Media.DATA;
+        String[] projection = {column};
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null && !cursor.isClosed())
+                cursor.close();
+        }
+        return null;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,6 +167,46 @@ public class ActivityMessage extends SwipeBackActivity {
         });
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(messageAdapter);
+        initialButtons();
 
+    }
+
+    private void initialButtons() {
+        btnFile = (ImageButton) findViewById(R.id.btn_message_file);
+        btnImage = (ImageButton) findViewById(R.id.btn_message_image);
+        btnSend = (ImageButton) findViewById(R.id.btn_message_send);
+        btnFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, REQUEST_FILE);
+            }
+        });
+        //     intent.setType(“image/*”);
+//intent.setType(“audio/*”); //选择音频
+//intent.setType(“video/*”); //选择视频 （mp4 3gp 是android支持的视频格式）
+//intent.setType(“video/*;image/*”);//同时选择视频和图片
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, REQUEST_FILE);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_FILE) {//是否选择，没选择就不会继续
+            Uri uri = data.getData();//得到uri，后面就是将uri转化成file的过程。
+            String file = getPhotoPathFromContentUri(ActivityMessage.this, uri);
+            Toast.makeText(ActivityMessage.this, file.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
